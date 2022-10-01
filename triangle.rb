7f7482f500da1a3815b43a7148163607a55e3536
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2022-09-30 21:06:46 -0700

    completed triangle.rb

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  result = if a == 0 || b == 0 || c == 0
+    raise TriangleError
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError
+  elsif a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError
+  elsif a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
+  return result
 end
 
 # Error class used in part 2.  No need to change this code.
